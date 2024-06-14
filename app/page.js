6d87202e130bee2cc6765d0628ea0133f7a1486d
Daniel Kuznetsov <danilgkuznet@icloud.com>
2024-06-14 10:31:38 -0700

    add and test supabase db access

--- app/page.js
+++ app/page.js
@@ -1,6 +1,11 @@
 import Image from "next/image";
+import supabase from "@/utils/supabaseClient";
 
-export default function Home() {
+export default async function Home() {
+  const { data, error } = await supabase.from("notes").select("*");
+
+  console.log(data, error);
+
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
--- utils/supabaseClient.js
+++ utils/supabaseClient.js
@@ -1,9 +1,7 @@
 import { createClient } from "@supabase/supabase-js";
 
-const supabaseUrl = "https://grnyepdqhnkpommddadd.supabase.co";
-const supabaseAnonKey =
-  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImdybnllcGRxaG5rcG9tbWRkYWRkIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTgzODI2NzIsImV4cCI6MjAzMzk1ODY3Mn0.y2_BGGQAf1lWuIZyFjkGy_o8OqyNBZfz52zvB9G7RM8";
-
+const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL;
+const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY;
 const supabase = createClient(supabaseUrl, supabaseAnonKey);
 
 export default supabase;
