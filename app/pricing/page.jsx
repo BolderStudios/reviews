4c452a35896a4b3eac979fbeee1c94154656c8c6
Daniel Kuznetsov <danilgkuznet@icloud.com>
2024-06-30 13:55:11 -0700

    webpack: clear npm cache and add error handling in middleware

---
+++ app/pricing/page.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { auth } from "@clerk/nextjs/server";
+import { redirect } from "next/navigation";
+
+export default async function Page() {
+  const { userId } = await auth();
+
+  if (userId) {
+    redirect("/dashboard");
+  }
+  return <div>Pricing</div>;
+}
--- middleware.js
+++ middleware.js
@@ -9,28 +9,40 @@ const isProtectedRoute = createRouteMatcher([
   "/api/create-checkout-session",
 ]);
 
-export default clerkMiddleware((auth, req) => {
-  const url = req.nextUrl;
+export default clerkMiddleware(async (auth, req) => {
+  try {
+    const url = req.nextUrl;
 
-  // Handle protected routes
-  const { userId, sessionClaims } = auth();
-  console.log("userId:", userId);
-  if (isProtectedRoute(req)) {
-    if (!userId) {
-      const signInUrl = new URL("/sign-in", req.url);
-      return NextResponse.redirect(signInUrl);
+    // Handle protected routes
+    if (isProtectedRoute(req)) {
+      const { userId, sessionClaims } = await auth();
+      console.log("userId:", userId);
+
+      if (!userId) {
+        const signInUrl = new URL("/sign-in", req.url);
+        return NextResponse.redirect(signInUrl);
+      }
+
+      // Check if onboarding is complete —> only works for protected pages
+      const onboardingComplete = sessionClaims?.metadata?.onboardingComplete;
+      console.log("onboardingComplete:", onboardingComplete);
+      console.log("url.pathname:", url.pathname);
+
+      if (onboardingComplete !== true && url.pathname !== "/onboarding") {
+        console.log("Redirecting to onboarding");
+        const onboardingUrl = new URL("/onboarding", req.url);
+        return NextResponse.redirect(onboardingUrl);
+      }
     }
-    // Check if onboarding is complete
-    const onboardingComplete = sessionClaims?.metadata?.onboardingComplete;
-    if (!onboardingComplete && url.pathname !== "/onboarding") {
-      const onboardingUrl = new URL("/onboarding", req.url);
-      return NextResponse.redirect(onboardingUrl);
-    }
+
+    return NextResponse.next();
+  } catch (error) {
+    console.error("Middleware error:", error);
+    return NextResponse.next();
   }
-
-  return NextResponse.next();
 });
 
 export const config = {
-  matcher: ["/((?!_next/static|_next/image|favicon.ico).*)", "/api/(.*)"],
+  // matcher: ["/((?!_next/static|_next/image|favicon.ico).*)", "/api/(.*)"],
+  matcher: ["/((?!.*\\..*|_next).*)", "/", "/(api|trpc)(.*)"],
 };
