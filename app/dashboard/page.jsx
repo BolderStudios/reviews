438048291942262f01ce250698370c304990e079
Daniel Kuznetsov <danilgkuznet@icloud.com>
2024-06-30 20:41:16 -0700

    Add session claims checker in /app/dashboard

--- app/dashboard/page.jsx
+++ app/dashboard/page.jsx
@@ -1,7 +1,15 @@
 // app/dashboard/page.jsx
 
 import Dashboard from "@/components/Dashboard";
+import { auth } from "@clerk/nextjs/server";
+import { redirect } from "next/navigation";
 
 export default async function Page() {
+  const { sessionClaims } = await auth();
+
+  if (sessionClaims?.metadata.onboardingComplete === false) {
+    redirect("/onboarding");
+  }
+
   return <Dashboard />;
 }
