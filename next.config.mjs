87c91f1a0eaf9470794e11354b532f38cd7b92d8
Daniel Kuznetsov <danilgkuznet@icloud.com>
2024-06-30 21:37:06 -0700

    Remove unneeded code in config file

--- next.config.mjs
+++ next.config.mjs
@@ -5,23 +5,6 @@ const nextConfig = {
       bodySizeLimit: "5mb",
     },
   },
-  reactStrictMode: true,
-  poweredByHeader: false,
-  // Log webpack compilation errors
-  webpack: (config, { isServer, dev }) => {
-    if (!dev && !isServer) {
-      config.optimization.minimizer.push(
-        new (require("terser-webpack-plugin"))({
-          terserOptions: {
-            compress: {
-              drop_console: false,
-            },
-          },
-        })
-      );
-    }
-    return config;
-  },
 };
 
 export default nextConfig;
