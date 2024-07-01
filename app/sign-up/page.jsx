25e6bd4f03530d3328cc5c78de56d4b3eaa0ad5f
Daniel Kuznetsov <danilgkuznet@icloud.com>
2024-07-01 12:36:39 -0700

    Change redirect on sign up

--- app/sign-up/page.jsx
+++ app/sign-up/page.jsx
@@ -1,16 +1,24 @@
 // app/sign-up/[[...sign-up]]/page.jsx
 
 import CustomSignUp from "@/components/CustomSignUp";
-import { SignUp } from "@clerk/nextjs";
-import { auth } from "@clerk/nextjs/server";
-import { redirect } from "next/navigation";
+import {
+  SignUp
+} from "@clerk/nextjs";
+import {
+  auth
+} from "@clerk/nextjs/server";
+import {
+  redirect
+} from "next/navigation";
 
 export default async function Page() {
-  const { userId } = await auth();
+  const {
+    userId
+  } = await auth();
 
   if (userId) {
-    redirect("/");
+    redirect("/onboarding");
   }
 
-  return <CustomSignUp />;
+  return <CustomSignUp / > ;
 }
