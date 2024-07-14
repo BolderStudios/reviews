c0114dd4339b54cf59d91051238160482d238c57
Daniel Kuznetsov <danilgkuznet@icloud.com>
2024-07-13 21:16:29 -0700

    Remove unnecessary code in Keywords

--- components/Keywords.jsx
+++ components/Keywords.jsx
@@ -2,20 +2,17 @@
 
 import { useState, useEffect } from "react";
 import { Skeleton } from "./ui/skeleton";
-import { getKeywords, getCategories } from "@/utils/reviews";
+import { getCategories } from "@/utils/reviews";
 import { CategoryTabs } from "@/components/ui/CategoryTabs";
 
 export default function Keywords({ selectedLocation }) {
   const [isPageLoading, setIsPageLoading] = useState(false);
-  const [keywordsData, setKeywordsData] = useState(null);
   const [categoriesData, setCategoriesData] = useState(null);
 
   useEffect(() => {
     const fetchDashboardData = async () => {
       setIsPageLoading(true);
-      const keywords = await getKeywords(selectedLocation.id);
       const categories = await getCategories(selectedLocation.id);
-      setKeywordsData(keywords);
       setCategoriesData(categories);
 
       setIsPageLoading(false);
@@ -28,7 +25,7 @@ export default function Keywords({ selectedLocation }) {
       <div className="flex items-center justify-between mb-6">
         <h2 className="font-bold text-2xl">Extracted Keywords</h2>
       </div>
-      {isPageLoading || keywordsData === null ? (
+      {isPageLoading || categoriesData === null ? (
         <div className="mt-6 space-y-6">
           <Skeleton className="h-[200px] w-full" />
           <Skeleton className="h-[100px] w-full" />
