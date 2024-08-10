d519ddaa090a4d30ad9910acb1cf3ddf3eda7ddc
Daniel Kuznetsov <danilgkuznet@icloud.com>
2024-08-10 16:16:37 -0700

    Update loader in ClientRedirect

--- components/ui/ClientRedirect.jsx
+++ components/ui/ClientRedirect.jsx
@@ -7,10 +7,12 @@ import LoadingSpinner from "./LoadingSpinner";
 
 export default function ClientRedirect({ redirect_url }) {
   const router = useRouter();
+  const [isLoaded, setIsLoaded] = useState(false);
 
   useEffect(() => {
     router.push(redirect_url);
+    setIsLoaded(true);
   }, [redirect_url, router]);
 
-  return <LoadingSpinner />;
+  return isLoaded ? <LoadingSpinner /> : null;
 }
