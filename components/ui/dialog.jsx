8fd6fe2c13eae1437675c842be168e3701da79a6
Daniel Kuznetsov <danilgkuznet@icloud.com>
2024-08-18 22:42:24 -0700

    Update overlay opacity

--- components/ui/dialog.jsx
+++ components/ui/dialog.jsx
@@ -20,7 +20,7 @@ const DialogOverlay = React.forwardRef(
       ref={ref}
       className={cn(
         `fixed inset-0 z-50 ${
-          lightOverlay ? "bg-black/10" : "bg-black/80"
+          lightOverlay ? "bg-black/20" : "bg-black/80"
         } data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=open]:fade-in-0`,
         className
       )}
