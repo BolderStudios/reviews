c3362ee92cd71e6cc2ad3c0c7e1f3be17a95503e
Daniel Kuznetsov <danilgkuznet@icloud.com>
2024-08-29 20:23:36 -0700

    Comment out privacy/policy and terms of service links for now

--- components/ui/MainPage/Footer.jsx
+++ components/ui/MainPage/Footer.jsx
@@ -6,8 +6,8 @@ import Logo from "@/public/logoBlack.png";
 export default function Footer() {
     return (
         <footer className="not-prose flex flex-col sm:flex-row items-center justify-center pb-[2rem] max-w-[800px] mx-auto px-[1.6rem]">
-            <div className="grid gap-6">
-                <div className="grid gap-6">
+            <div className="grid gap-4">
+                <div className="grid gap-4">
                     <Link href="/">
                         <Image
                             src={Logo}
@@ -20,10 +20,10 @@ export default function Footer() {
                     <p className="text-stone-900 text-base leading-normal tracking-tight">
                         BrandArmor is a platform that allows local businesses to offer prepaid credit to their customers. We help you get more customers, keep them longer, and make more money.
                     </p>
-                    <div className="flex mb-2 gap-4 text-sm text-muted-foreground underline underline-offset-4 md:mb-0 md:flex-row">
+                    {/* <div className="flex mb-2 gap-4 text-sm text-muted-foreground underline underline-offset-4 md:mb-0 md:flex-row">
                         <Link href="/privacy-policy">Privacy Policy</Link>
                         <Link href="/terms-of-service">Terms of Service</Link>
-                    </div>
+                    </div> */}
                     <p className="text-muted-foreground text-xs ">
                         ©{" "}
                         <a href="https://www.bolderstudios.com/" className="underline">Bolder Studios LLC</a>
