cf76c53d29c35e91d91b0a5ceab9a9d7df12ae3e
Daniel Kuznetsov <danilgkuznet@icloud.com>
2024-08-30 17:33:17 -0700

    Update robots.js back to its old state

--- app/robots.js
+++ app/robots.js
@@ -3,7 +3,7 @@ export default function robots() {
     rules: {
       userAgent: '*',
       allow: '/',
-    //   disallow: ["/api", "/sign-in", "/sign-up"],
+      disallow: ["/api", "/sign-in", "/sign-up", "/not-found"],
     },
     sitemap: `${process.env.NEXT_PUBLIC_SITE_URL}/sitemap.xml`,
   }
