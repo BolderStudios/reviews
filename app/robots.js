524085240525f92bf828105f2cae3a222e1351b8
Daniel Kuznetsov <danilgkuznet@icloud.com>
2024-08-30 17:28:57 -0700

    Update robots.js file

--- app/robots.js
+++ app/robots.js
@@ -3,7 +3,7 @@ export default function robots() {
     rules: {
       userAgent: '*',
       allow: '/',
-      disallow: ["/api", "/sign-in", "/sign-up"],
+    //   disallow: ["/api", "/sign-in", "/sign-up"],
     },
     sitemap: `${process.env.NEXT_PUBLIC_SITE_URL}/sitemap.xml`,
   }
